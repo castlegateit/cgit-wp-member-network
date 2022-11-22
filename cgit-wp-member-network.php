be495c50491bd347547c3cc247a20243fa2f4adc
John Hughes <john@castlegateit.co.uk>
2022-11-22 18:47:04 +0000

    Fix URL

--- cgit-wp-member-network.php
+++ cgit-wp-member-network.php
@@ -3,9 +3,9 @@
 /*
 
 Plugin Name: Castlegate IT WP Member Network
-Plugin URI:
+Plugin URI: https://github.com/castlegateit/cgit-wp-member-network
 Description: Basic member network plugin.
-Version: 1.0.0
+Version: 1.0.1
 Author: Castlegate IT
 Author URI: https://www.castlegateit.co.uk/
 Network: true
